805f2cbb3b543b7a4de1513412eb586fc738ccb0
Guilherme Rolemberg <guilherme.rolemberg@outlook.com>
2019-09-23 20:18:14 -0300

    1° versionamento

---
+++ TesteDev.Safra/CreditoImobiliario.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace TesteDev.Safra
+{
+    public class CreditoImobiliario
+    {
+        public enum EnumTipoCredito
+        {
+            Direto = 1,
+            Consignado = 2,
+            PessoaJuridica = 3,
+            PessoaFisica = 4,
+            Imobiliario = 5
+        }
+
+
+        public double ValorEmprestimo { get; set; }
+        public int QuantidadeParcelas { get; set; }
+        public DateTime DataVencimento { get; set; }
+
+
+        public double ValorTotalEmprestimo { get; set; }
+        public bool Status { get; set; }
+        public EnumTipoCredito TipoCredito { get; set; }
+
+        public double TaxaCredito
+        {
+            get
+            {
+                double tx = 0;
+                switch (TipoCredito)
+                {
+                    case EnumTipoCredito.Direto: tx = 2; break;
+                    case EnumTipoCredito.Consignado: tx = 1; break;
+                    case EnumTipoCredito.PessoaJuridica: tx = 5; break;
+                    case EnumTipoCredito.PessoaFisica: tx = 3; break;
+                    case EnumTipoCredito.Imobiliario: tx = (double)9 / (double)12; break;//divisão para "transformar" a a.a em a.m
+                }
+
+                return tx;
+            }
+        }
+        //Validação para o Valor Máximo do Crédito
+        public bool ValidarValorMaximoEmprestimo => ValorEmprestimo <= 1000000;
+        //Validação para a Quantidade de Parcelas
+        public bool ValidarQuantidadeMaxMinParcelas => (QuantidadeParcelas <= 72 && QuantidadeParcelas >= 5);
+        //Validação para o Valor Mínimo do Crédito quando Pessoa Juridica
+        public bool ValidarValorEmprestimoMinPJ => ValorEmprestimo >= 15000;
+
+        public double ValorTotalJuros => ValorTotalEmprestimo - ValorEmprestimo;
+
+        public bool ValidarDataVencimentoPrimeiraParcela()
+        {
+            TimeSpan diferenca = DataVencimento.Date - DateTime.Now.Date;
+
+            return diferenca.Days >= 15 && diferenca.Days <= 40;
+        }
+
+        public void CalucarValorTotalEmprestimo()
+        {
+            double valorJuros = (TaxaCredito / 100) * ValorEmprestimo;
+            double valorMensal = (ValorEmprestimo / QuantidadeParcelas) + valorJuros;
+
+            for (int i = 0; i < QuantidadeParcelas; i++)
+            {
+                ValorTotalEmprestimo += valorMensal;
+            }
+        }
+
+
+    }//class
+}//nanmespace
---
+++ TesteDev.Safra/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace TesteDev.Safra
+{
+    internal class Program
+    {
+        private static CreditoImobiliario creditoImobiliario;
+        private static bool valido = false;
+
+        private static void Main(string[] args)
+        {
+
+            creditoImobiliario = new CreditoImobiliario();
+
+            Console.WriteLine("Teste Desenvolvedor - Banco Safra \n");
+
+            valido = false;
+            ValidarInputTipoCredito();
+
+            valido = false;
+            ValidarInputValorEmprestimo();
+
+            valido = false;
+            ValidarInputQuantidadeParcelas();
+
+            valido = false;
+            ValidarInputDataVencimento();
+
+            creditoImobiliario.CalucarValorTotalEmprestimo();
+
+            Console.WriteLine("\nStatus do Crédto: Aprovado");
+
+            Console.WriteLine("Valor Total do Empréstimo com Juros: {0}", creditoImobiliario.ValorTotalEmprestimo.ToString("C", new System.Globalization.CultureInfo("pt-BR")));
+            Console.WriteLine("Valor Total do Juros: {0}", creditoImobiliario.ValorTotalJuros.ToString("C", new System.Globalization.CultureInfo("pt-BR")));
+
+            Console.ReadKey();
+        }
+
+        private static void ValidarInputTipoCredito()
+        {
+            while (!valido)
+            {
+                Console.WriteLine("Favor informar o Tipo do Crédito");
+                Console.WriteLine("1 - Crédito Direto, 2 - Crédito Consignado, 3 - Crédito Pessoa Juridica, 4 - Credito Pessoa Fisica, 5 - Crédito Imobiliario");
+
+                if (!int.TryParse(Console.ReadLine(), out int valor))
+                {
+                    Console.WriteLine("O Tipo do Crédito deve ser numérico");
+                }
+                else
+                {
+                    if (valor > 5)
+                    {
+                        Console.WriteLine("O Tipo de Crédito deve ser de acordo com os tipos informados");
+                    }
+                    else
+                    {
+                        creditoImobiliario.TipoCredito = (CreditoImobiliario.EnumTipoCredito)valor;
+                        valido = true;
+                    }
+                }
+            }
+        }
+
+        private static void ValidarInputValorEmprestimo()
+        {
+            while (!valido)
+            {
+                Console.WriteLine("Favor informar o Valor do Crédito");
+
+                if (!double.TryParse(Console.ReadLine(), out double valor))
+                {
+                    Console.WriteLine("O Valor do Empréstimo deve ser numérico");
+                }
+                else
+                {
+                    creditoImobiliario.ValorEmprestimo = valor;
+
+                    if (!creditoImobiliario.ValidarValorMaximoEmprestimo)
+                    {
+                        Console.WriteLine("Status do Crédito: Recusado \nO Valor Máximo para o Empréstimo é de: R$ 1.000.000,00");
+                    }
+                    else if (creditoImobiliario.TipoCredito == CreditoImobiliario.EnumTipoCredito.PessoaJuridica && !creditoImobiliario.ValidarValorEmprestimoMinPJ)
+                    {
+                        Console.WriteLine("Status do Crédito: Recusado \nO Valor Mínimo para o Crédito Pessoa Juridica é de: R$ 15.000,00");
+                    }
+                    else
+                    {
+                        valido = true;
+                    }
+                }
+            }
+        }
+
+        private static void ValidarInputQuantidadeParcelas()
+        {
+            while (!valido)
+            {
+                Console.WriteLine("Favor informar a Quantidade de Parcelas");
+
+                if (!int.TryParse(Console.ReadLine(), out int valor))
+                {
+                    Console.WriteLine("Status do Crédito: Recusado \nA Quantidade de Parcelas deve ser numérico");
+                }
+                else
+                {
+                    creditoImobiliario.QuantidadeParcelas = valor;
+
+                    if (!creditoImobiliario.ValidarQuantidadeMaxMinParcelas)
+                    {
+                        Console.WriteLine("Status do Crédito: Recusado \nA Quantidade de Parcelas deve ser entre 5 e 72");
+                    }
+                    else
+                    {
+                        valido = true;
+                    }
+                }
+            }
+        }
+
+        private static void ValidarInputDataVencimento()
+        {
+            while (!valido)
+            {
+                Console.WriteLine("Favor informar a Data do Primeiro Vencimento");
+
+                if (!DateTime.TryParse(Console.ReadLine(), out DateTime data))
+                {
+                    Console.WriteLine("Status do Crédito: Recusado \nA Data do Primeiro Vencimento deve ser no formato: DD/MM/AAAA");
+                }
+                else
+                {
+                    creditoImobiliario.DataVencimento = data;
+
+                    if (!creditoImobiliario.ValidarDataVencimentoPrimeiraParcela())
+                    {
+                        Console.WriteLine("Status do Crédito: Recusado \nA Primeira Data de Vecimento deve ser no mínimo D+15 e no méximo D+ 40");
+                    }
+                    else
+                    {
+                        valido = true;
+                    }
+                }
+            }
+        }
+
+
+    }//class
+}//namespace
